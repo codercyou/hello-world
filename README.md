fc450f298751e2a1f3e7b3fef3722c35f982e499
xuxingping <xuxingping@cyou-inc.com>
2019-07-03 18:26:37 +0800

    内置h2数据库存入用户名 && mysql对应操作

--- README.md
+++ README.md
@@ -6,6 +6,20 @@ git push -u origin master
 
 git commit --amend --no-edit  追加
 
+##脚本
+```sql
+
+CREATE TABLE user
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    account_id VARCHAR(100),
+    name VARCHAR(50),
+    token CHAR(36),
+    gmt_create BIGINT,
+    gmt_modified BIGINT
+);
+
+
 
 
 
--- src/main/java/com/changyou/community/controller/AuthorizeController.java
+++ src/main/java/com/changyou/community/controller/AuthorizeController.java
@@ -2,12 +2,18 @@ package com.changyou.community.controller;
 
 import com.changyou.community.dto.AccessTokenDTO;
 import com.changyou.community.dto.GithubUser;
+import com.changyou.community.dto.User;
+import com.changyou.community.mapper.UserMapper;
 import com.changyou.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -24,8 +30,12 @@ public class AuthorizeController {
 
     @Autowired
     private GitHubProvider gitHubProvider;
+
+    @Autowired
+    UserMapper userMapper;
+
     @GetMapping("/callback")
-    public String callback(String code, String state, Model model){
+    public String callback(String code, String state, Model model, HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id("fb8df772723c425b9d3a");
         accessTokenDTO.setClient_secret("79d3017a50fc3223d49065cfc0e39e02f74e515c");
@@ -39,7 +49,24 @@ public class AuthorizeController {
         System.out.println(githubUser);
 
         model.addAttribute("user",githubUser);
-        return "index";
+
+        if(githubUser!=null){
+            //登录成功
+            request.getSession().setAttribute("user",githubUser);
+            User user = new User();
+            user.setName(githubUser.getName());
+            user.setAccountId(Long.toString(githubUser.getId()));
+            user.setToken(UUID.randomUUID().toString());
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+
+            userMapper.insert(user);
+            return "redirect:/";
+        }else{
+            return "redirect:/";
+            //登录失败
+        }
+        //return "index";
     }
 
 
